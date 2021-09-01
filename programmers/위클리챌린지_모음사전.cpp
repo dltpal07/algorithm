fa57051cbd03ea664039bc97cc78ce938d37d2fa
dltpal07 <dltpal07@gmail.com>
2021-09-01 23:09:45 +0900

    Add source

---
+++ programmers/위클리챌린지_모음사전.cpp
@@ -0,0 +1,36 @@
+//https://programmers.co.kr/learn/courses/30/lessons/84512
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+int solution(string word)
+{
+	int answer = 0;
+	int mul[5] = { 1, 5, 25,125, 625 };
+	map<char, int> m = { {'A', 0}, {'E', 1}, {'I', 2}, {'O', 3}, {'U', 4} };
+	for (int i = 0; i < word.size(); i++)
+	{
+		answer += 1;
+		for (int j = 0; j < m[word[i]]; j++)
+		{
+			for (int k = 4; k >= i; k--)
+			{
+				answer += mul[4 - k];
+			}
+		}
+
+
+	}
+	return answer;
+}
+
+int main(void)
+{
+	string word1 = "AAAAE", word2 = "AAAE", word3 = "I", word4 = "EIO";
+	cout << solution(word4);
+	return 0;
+}
